c037d1adc3115a29eb1118656618a8c649a19ea9
Ulan Djamanbalaev <udjamanbalaev@gmail.com>
2019-11-29 11:36:26 +0600

    Implement sign out Render JSON responses using jbuilder

--- app/controllers/v1/sessions_controller.rb
+++ app/controllers/v1/sessions_controller.rb
@@ -1,15 +1,20 @@
 class V1::SessionsController < ApplicationController
   def create
-    user = User.find_by(email: params[:email])
+    @user = User.find_by(email: params[:email])
 
-    if user&.valid_password?(params[:password])
-      render json: user.as_json(only: [:email, :authentication_token]), status: :created
+    if @user&.valid_password?(params[:password])
+      render :create, status: :created
     else
       head(:unauthorized)
     end
   end
 
   def destroy
-
+    current_user&.authentication_token = nil
+    if current_user&.save
+      head(:ok)
+    else
+      head(:unauthorized)
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  namespace :v1 do
+  namespace :v1, defaults: { format: :json } do
     resources :contacts
-    resources :sessions, only: [:create, :destroy]
+    resource :sessions, only: [:create, :destroy]
   end
 end
