8cbed20ef0ba6686bb07b7f99f5cbad14a4b36a6
Ulan Djamanbalaev <udjamanbalaev@gmail.com>
2019-12-01 16:25:03 +0600

    Add relation between user and contacts Return only the required user's data

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :v1, defaults: { format: :json } do
     resources :contacts
-    resource :sessions, only: [:create, :destroy]
+    resource :sessions, only: [:create, :destroy, :show]
   end
 end
