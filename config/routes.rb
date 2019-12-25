ca66382d09be2ceb53efd41cfa06e2bb07e908ff
Ulan Djamanbalaev <udjamanbalaev@gmail.com>
2019-12-25 15:17:14 +0600

    Create Account manage endpoints

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :v1, defaults: { format: :json } do
-    resources :contacts
+    resources :accounts, only: [:create, :update] do
+      resources :contacts
+    end
     resource :sessions, only: [:create, :destroy, :show]
     resources :users, only: [:create]
   end
